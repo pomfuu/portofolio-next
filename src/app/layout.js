c94aaaa17da5534ffccd793ca58c10071869b533
pomff <alincahyono@gmail.com>
2025-01-20 21:48:59 +0700

    bjir

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
+  variable: "--font-lato",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  style: ["normal", "italic"],
 });
 
 export const metadata = {
@@ -20,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.variable} antialiased`}
       >
         {children}
       </body>
